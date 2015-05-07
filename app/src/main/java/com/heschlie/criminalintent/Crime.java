0122566feac0169b1bde7b17c921dac4c7f05920
Stephen Schlie <heschlie@gmail.com>
2015-05-06 20:08:13 -0700

    - Chapter 17, working with file IO

--- app/src/main/java/com/heschlie/criminalintent/Crime.java
+++ app/src/main/java/com/heschlie/criminalintent/Crime.java
@@ -3,6 +3,9 @@ package com.heschlie.criminalintent;
 import android.content.Context;
 import android.text.format.DateFormat;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
@@ -11,6 +14,11 @@ import java.util.UUID;
  * Created by heschlie on 4/28/15.
  */
 public class Crime {
+    private static final String JSON_ID = "id";
+    private static final String JSON_TITLE = "title";
+    private static final String JSON_SOLVED = "solved";
+    private static final String JSON_DATE = "date";
+
     private UUID mId;
     private String mTitle;
     private Date mDate;
@@ -21,6 +29,25 @@ public class Crime {
         mId = UUID.randomUUID();
         mDate = new Date();
         mContext = context.getApplicationContext();
+    }
+
+    public Crime(Context c, JSONObject json) throws JSONException {
+        mContext = c;
+        mId = UUID.fromString(json.getString(JSON_ID));
+        if (json.has(JSON_TITLE)) {
+            mTitle = json.getString(JSON_TITLE);
+        }
+        mSolved = json.getBoolean(JSON_SOLVED);
+        mDate = new Date(json.getLong(JSON_DATE));
+    }
+
+    public JSONObject toJSON() throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put(JSON_ID, mId.toString());
+        json.put(JSON_TITLE, mTitle);
+        json.put(JSON_SOLVED, mSolved);
+        json.put(JSON_DATE, mDate.getTime());
+        return json;
     }
 
     public UUID getId() {
--- app/src/main/java/com/heschlie/criminalintent/CrimeLab.java
+++ app/src/main/java/com/heschlie/criminalintent/CrimeLab.java
@@ -1,6 +1,7 @@
 package com.heschlie.criminalintent;
 
 import android.content.Context;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.UUID;
@@ -9,13 +10,23 @@ import java.util.UUID;
  * Created by heschlie on 4/30/15.
  */
 public class CrimeLab {
+    private static final String TAG = "CrimeLab";
+    private static final String FILENAME = "crimes.json";
     private static CrimeLab sCrimeLab;
     private Context mAppContext;
     private ArrayList<Crime> mCrimes;
+    private CriminalIntentJSONSerializer mSerializer;
 
     private CrimeLab(Context appContext) {
         mAppContext = appContext;
-        mCrimes = new ArrayList<Crime>();
+        mSerializer = new CriminalIntentJSONSerializer(mAppContext, FILENAME);
+
+        try {
+            mCrimes = mSerializer.loadCrimes();
+        } catch (Exception e) {
+            mCrimes = new ArrayList<Crime>();
+            Log.e(TAG, "Error loading crimes", e);
+        }
     }
 
     public static CrimeLab get(Context c) {
@@ -23,6 +34,17 @@ public class CrimeLab {
             sCrimeLab = new CrimeLab(c.getApplicationContext());
         }
         return sCrimeLab;
+    }
+
+    public boolean saveCrimes() {
+        try {
+            mSerializer.saveCrimes(mCrimes);
+            Log.d(TAG, "Crimes saved to file");
+            return true;
+        } catch (Exception e) {
+            Log.e(TAG, "Error saving crimes", e);
+            return false;
+        }
     }
 
     public ArrayList<Crime> getCrimes() {
